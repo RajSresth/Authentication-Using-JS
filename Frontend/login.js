b60eb457fc45ce7a708de315368680cd695e1424
Shresth Rajput <rajshresth2@gmail.com>
2025-07-31 14:23:35 +0530

    Captcha Code Logic

--- Frontend/login.js
+++ Frontend/login.js
@@ -1,13 +1,54 @@
 import axios from "axios";
+import 'animate.css';
+
 const API_Url="http://localhost:3000/users";
 
-
 const loginForm=document.querySelector("form");
+const displayCaptcha=document.querySelector(".displayCaptcha");
+const refreshCaptcha= document.querySelector(".refreshCaptcha")
+let captcha="";
+
+// generate captcha
+const generateCaptcha=()=>{
+    const char="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    captcha="";
+
+    for(let i=0; i<6;i++)
+    {
+        captcha += char[Math.floor(Math.random() * char.length)]
+    }
+   displayCaptcha.textContent=captcha;
+}
+
+generateCaptcha()
+
+// refreshCaptcha using animate.css library 
+refreshCaptcha.addEventListener("click",()=>{
+    generateCaptcha();
+    refreshCaptcha.className+=" animate__rotateOut";
+})
+
+refreshCaptcha.addEventListener("animationend",()=>{
+    // Remove old animation
+  refreshCaptcha.classList.remove("animate__rotateOut");
+})
+
+
+
+
+
 
 loginForm.addEventListener("submit",async (e)=>{
+    
     e.preventDefault();
+    const rawData=Object.fromEntries(new FormData(loginForm).entries());
 
-    const user=Object.fromEntries(new FormData(loginForm).entries());
+    
+    const user=Object.fromEntries(
+        Object.entries(rawData).map(([key,value])=> [key,value.trim()])
+    )
+
+  
 
     const {data}=await axios.get(`${API_Url}?email=${user.email}&password=${user.password}`);
 

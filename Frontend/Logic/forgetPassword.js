405815510db15434c0b970b85a0e938911a57d6c
Shresth Rajput <rajshresth2@gmail.com>
2025-08-07 10:41:45 +0530

    Email Js Part-2

--- Frontend/Logic/forgetPassword.js
+++ Frontend/Logic/forgetPassword.js
@@ -1,76 +1,140 @@
-import axios from 'axios';
-import emailjs from '@emailjs/browser';
+import axios from "axios";
+import emailjs from "@emailjs/browser";
 const API_Url = "http://localhost:3000/users";
 
-
 // Initialize public key
-emailjs.init('GT03x6FWyzIwPMEzw');
+emailjs.init("GT03x6FWyzIwPMEzw");
 
-const serviceId="service_68z835w";
-const templateId="template_316ymmh";
+const serviceId = "service_68z835w";
+const templateId = "template_316ymmh";
 
+const emailVerificationForm = document.querySelector("#emailVerificationForm");
+const email = document.querySelector("#email");
+const sendOtp = document.querySelector("#sendOtp");
+const sendVerification = document.querySelector("#sendVerification");
+const otpVerificationForm = document.querySelector("#otpVerificationForm");
+const otpInput = document.querySelector("#otpInput");
+const timer = document.querySelector("#timer");
+const confirmOtpBtn = document.querySelector("#confirmOtpBtn");
+const resendOtpBtn = document.querySelector("#resendOtpBtn");
+const resetPasswordForm = document.querySelector("#resetPasswordForm");
 
-const emailVerificationForm=document.querySelector("#emailVerificationForm");
-const email=document.querySelector("#email");
-const sendOtp=document.querySelector("#sendOtp");
-const sendVerification=document.querySelector("#sendVerification")
-const otpVerificationForm=document.querySelector("#otpVerificationForm");
-const otpInput=document.querySelector("#otpInput");
-const timer=document.querySelector("#timer");
-const confirmOtpBtn=document.querySelector("#confirmOtpBtn");
-const resendOtpBtn=document.querySelector("#resendOtpBtn");
-
-
-
+const resetPasswordBtn = document.querySelector("#resetPasswordBtn");
 
 // Global Declaration
-let userId=null;
-let getOtp;
+let userId = null;
+let getOtp = "";
+let otpExpiryTime;
+let intervalId;
 
-emailVerificationForm.addEventListener("submit",async (e)=>{
-    e.preventDefault() // stop reload
-    const emailValue=email.value;
+emailVerificationForm.addEventListener("submit", async (e) => {
+  e.preventDefault(); // stop reload
+  const emailValue = email.value;
 
-    try {
-        const {data}= await axios.get(`${API_Url}?email=${emailValue}`)
+  try {
+    const { data } = await axios.get(`${API_Url}?email=${emailValue}`);
 
-        if(data.lenth === 0)
-        {
-            alert("User doesn't Exist!");
-            return 
-        }
-
-        userId=data[0].id;
-        sendOtp.style.display="block"
-        email.disabled=true;
-        email.style.cursor="not-allowed";
-        sendVerification.disabled=true;
-        alert("Email Verification Successfull..!")
-    } catch (error) {
-        
+    if (data.lenth === 0) {
+      alert("Email not found!");
+      return;
     }
-})
 
+    userId = data[0].id;
+    sendOtp.style.display = "block";
+    email.disabled = true;
+    email.style.cursor = "not-allowed";
+    sendVerification.disabled = true;
+    alert("Email Verification Successfull..!");
+  } catch (error) {
+    console.error("Error verifying email:", err);
+    alert("Something went wrong.");
+  }
+});
 
+function startTimer(duration) {
+  let time = duration;
+  intervalId = setInterval(() => {
+    let minute = String(Math.floor(time / 60));
+    let second = String(time % 60);
+    timer.textContent = `${minute.padStart(2, "0")}:${second.padStart(2, "0")}`;
+    -1;
+    if (--time < 0) {
+      timer.textContent = `OTP Expired`;
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
 
-sendOtp.addEventListener("click",async ()=>{
-        
-        sendOtp.style.display="none";
-        emailVerificationForm.style.display="none";
-        otpVerificationForm.style.display="block";
+sendOtp.addEventListener("click", async () => {
+  emailVerificationForm.style.display = "none";
 
+  // Generate 6 digit otp;
+  getOtp = Math.floor(100000 + Math.random() * 900000);
 
-        // Generate 6 digit otp;
-        getOtp=Math.floor(100000 + Math.random() * 900000);
+  // Otp expiry time
+  otpExpiryTime = Date.now() + 2 * 60 * 1000;
 
-        emailjs.send(serviceId, templateId,{
-            email:email.value,
-            passcode:getOtp,
-            time:"2 minutes"
-        }).then(() => {
-            alert("OTP Sent successfull to your verified email");
-        }).catch((error) => {
-            alert("Failed to send OTP. Please try again.");
-        });
-        
-})
+  emailjs
+    .send(serviceId, templateId, {
+      email: email.value,
+      passcode: getOtp,
+      time: "2 minutes",
+    })
+    .then(() => {
+      alert("OTP Sent successfull to your verified email");
+      sendOtp.style.display = "none";
+      otpVerificationForm.style.display = "block";
+      // start timer function
+      startTimer(120);
+    })
+    .catch((error) => {
+      alert("Failed to send OTP. Please try again.");
+    });
+});
+
+// Resend otp button
+resendOtpBtn.addEventListener("click", () => {
+  sendOtp.click();
+});
+
+confirmOtpBtn.addEventListener("click", () => {
+  // time comparison
+  if (Date.now() > otpExpiryTime) {
+    alert("Otp is Expired");
+    return;
+  }
+
+  const enteredOtp = otpInput.value;
+  console.log(getOtp);
+  // Otp value comparison
+  if (getOtp == enteredOtp) {
+    otpVerificationForm.style.display = "none";
+    resetPasswordForm.style.display = "flex";
+  } else {
+    alert("Please enter correct OTP");
+    resendOtpBtn.disabled = "false";
+  }
+});
+
+// resetPasswordForm
+resetPasswordForm.addEventListener("submit", async (e) => {
+    e.preventDefault
+  const newPassword = document.querySelector("#newPassword").value;
+  const confirmPassword = document.querySelector("#confirmPassword").value;
+
+  if (newPassword !== confirmPassword) {
+    alert("Password does not match");
+    return;
+  }
+
+  try {
+    axios.patch(`${API_Url}/id=${userId}`, {
+      password: newPassword,
+    });
+
+    alert("Password reset successfully");
+    window.location.href = "login.html";
+  } catch (err) {
+    alert("Something wen Wrong...");
+  }
+});
